ffb6fe64c79b6710fbfcec95d4ee5d453aa19a87
andreysulymka <andreysulymka5@gmail.com>
2023-11-24 14:46:01 +0200

    add all functional to my project

--- my-app/src/components/app-filter/app-filter.js
+++ my-app/src/components/app-filter/app-filter.js
@@ -1,19 +1,24 @@
 import "./app-filter.css";
 
-const AppFilter = () => {
-  return (
-    <div className="btn-group">
-      <button className="btn btn-info" type="button">
-        Всі співробітники
+const AppFilter = (props) => {
+  const buttonsData = [
+    { name: "all", label: "Всі співробітники" },
+    { name: "rise", label: "Cпівробітники на підвищення" },
+    { name: "moreThen1000", label: "З/п більше 1000$" },
+  ];
+
+  const buttons = buttonsData.map(({ name, label }) => {
+    const active = props.filter === name;
+    const clazz = active ? "btn-info" : "btn-outline-info";
+    return (
+      <button className={`btn ${clazz}`} type="button"
+        key={name}
+        onClick={() => props.onFilterSelect(name)}>
+        {label}
       </button>
-      <button className="btn btn-outline-info" type="button">
-        Співробітники на підвищення
-      </button>
-      <button className="btn btn-outline-info" type="button">
-        З/п більше 1000$
-      </button>
-    </div>
-  );
+    );
+  });
+  return <div className="btn-group">{buttons}</div>;
 };
 
 export default AppFilter;
--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -17,6 +17,8 @@ class App extends Component {
         { name: "Alex M", salary: 3000, increase: true, rise: true, id: 2 },
         { name: "Gustav G", salary: 4000, increase: true, rise: false, id: 3 },
       ],
+      term: "",
+      filter: 'all'
     };
   }
 
@@ -45,19 +47,51 @@ class App extends Component {
     }));
   };
 
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter((elem) => {
+      return elem.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
+
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case 'rise':
+        return items.filter(item => item.rise);
+      case 'moreThen1000':
+        return items.filter(item => item.salary > 1000);
+      default:
+        return items
+    }
+  };
+
+  onFilterSelect = (filter) => {
+    this.setState({filter})
+  }
+
   render() {
+    const { data, term, filter} = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
         <div className="search-panel">
-          <SearchPanel />
-          <AppFilter />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}           
+          />
         </div>
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
         />
